73b68019d453a423c8eafc5d6400d2b8414fa018
javadansari65 <rayano_co@yahoo.com>
2025-04-26 12:38:06 +0330

    Update14040206_4

--- ShiftYar.Application/Features/UserModel/Services/UserService.cs
+++ ShiftYar.Application/Features/UserModel/Services/UserService.cs
@@ -17,12 +17,17 @@ namespace ShiftYar.Application.Features.UserModel.Services
     public class UserService : IUserService
     {
         private readonly IRepository<User> _repository;
+        private readonly IRepository<UserPhoneNumber> _repositoryPhoneNumber;
+        private readonly IRepository<UserRole> _repositoryUserRole;
         private readonly IMapper _mapper; // AutoMapper
 
-        public UserService(IRepository<User> repository, IMapper mapper)
+        public UserService(IRepository<User> repository, IRepository<UserPhoneNumber> repositoryPhoneNumber, 
+                                IRepository<UserRole> repositoryUserRole, IMapper mapper)
         {
             _repository = repository;
             _mapper = mapper;
+            _repositoryPhoneNumber = repositoryPhoneNumber;
+            _repositoryUserRole = repositoryUserRole;
         }
 
         public async Task<ApiResponse<List<UserDtoGet>>> GetFilteredUsersAsync(UserFilter filter)
@@ -78,13 +83,12 @@ namespace ShiftYar.Application.Features.UserModel.Services
                 // If the list is empty, remove all phone numbers
                 if (dto.OtherPhoneNumbers.Count == 0)
                 {
-                    // Get all phone numbers to delete
-                    var phoneNumbersToDelete = user.OtherPhoneNumbers.ToList();
-                    foreach (var phoneNumber in phoneNumbersToDelete)
+                    foreach (var phoneNumber in user.OtherPhoneNumbers.ToList())
                     {
-                        user.OtherPhoneNumbers.Remove(phoneNumber);
-                        await _repository.SaveAsync();
+                        _repositoryPhoneNumber.Delete(phoneNumber);
                     }
+                    await _repositoryPhoneNumber.SaveAsync();
+
                 }
                 else
                 {
@@ -95,9 +99,9 @@ namespace ShiftYar.Application.Features.UserModel.Services
 
                     foreach (var phoneNumber in phoneNumbersToRemove)
                     {
-                        user.OtherPhoneNumbers.Remove(phoneNumber);
-                        await _repository.SaveAsync();
+                        _repositoryPhoneNumber.Delete(phoneNumber);
                     }
+                    await _repositoryPhoneNumber.SaveAsync();
 
                     // Add new phone numbers
                     foreach (var phoneNumber in dto.OtherPhoneNumbers)
@@ -106,7 +110,7 @@ namespace ShiftYar.Application.Features.UserModel.Services
                         {
                             var newPhoneNumber = new UserPhoneNumber { PhoneNumber = phoneNumber, IsActive = true };
                             user.OtherPhoneNumbers.Add(newPhoneNumber);
-                            await _repository.SaveAsync();
+                            //await _repository.SaveAsync();
                         }
                     }
                 }
@@ -118,13 +122,11 @@ namespace ShiftYar.Application.Features.UserModel.Services
                 // If the list is empty, remove all user roles
                 if (dto.UserRoles.Count == 0)
                 {
-                    // Get all user roles to delete
-                    var userRolesToDelete = user.UserRoles.ToList();
-                    foreach (var userRole in userRolesToDelete)
+                    foreach (var userRole in user.UserRoles.ToList())
                     {
-                        user.UserRoles.Remove(userRole);
-                        await _repository.SaveAsync();
+                        _repositoryUserRole.Delete(userRole);
                     }
+                    await _repositoryUserRole.SaveAsync();
                 }
                 else
                 {
@@ -135,9 +137,9 @@ namespace ShiftYar.Application.Features.UserModel.Services
 
                     foreach (var userRole in userRolesToRemove)
                     {
-                        user.UserRoles.Remove(userRole);
-                        await _repository.SaveAsync();
+                        _repositoryUserRole.Delete(userRole);
                     }
+                    await _repositoryUserRole.SaveAsync();
 
                     // Add new user roles
                     foreach (var roleId in dto.UserRoles)
@@ -146,7 +148,7 @@ namespace ShiftYar.Application.Features.UserModel.Services
                         {
                             var newUserRole = new UserRole { RoleId = roleId, UserId = user.Id };
                             user.UserRoles.Add(newUserRole);
-                            await _repository.SaveAsync();
+                            //await _repository.SaveAsync();
                         }
                     }
                 }
@@ -158,7 +160,7 @@ namespace ShiftYar.Application.Features.UserModel.Services
             await _repository.SaveAsync();
 
             var result = _mapper.Map<UserDtoAdd>(user);
-            return ApiResponse<UserDtoAdd>.Success(result, "ویرایش انجام شد.");
+            return ApiResponse<UserDtoAdd>.Success(result, "ویرایش کاربر با موفقیت انجام شد.");
         }
 
 
@@ -167,34 +169,30 @@ namespace ShiftYar.Application.Features.UserModel.Services
             var user = await _repository.GetByIdAsync(id, "OtherPhoneNumbers", "UserRoles", "UserRoles.Role");
             if (user == null) return ApiResponse<string>.Fail("کاربر یافت نشد.");
 
-            //Remove all phone numbers user
+            //Delete all phone numbers of user
             if (user.OtherPhoneNumbers.Count > 0)
             {
-                // Get all phone numbers to delete
-                var phoneNumbersToDelete = user.OtherPhoneNumbers.ToList();
-                foreach (var phoneNumber in phoneNumbersToDelete)
+                foreach (var phoneNumber in user.OtherPhoneNumbers.ToList())
                 {
-                    user.OtherPhoneNumbers.Remove(phoneNumber);
-                    await _repository.SaveAsync();
+                    _repositoryPhoneNumber.Delete(phoneNumber);
                 }
+                await _repositoryPhoneNumber.SaveAsync();
             }
 
-            //Remove all user roles
+            //Delete all user roles
             if (user.UserRoles.Count > 0)
             {
-                // Get all user roles to delete
-                var userRolesToDelete = user.UserRoles.ToList();
-                foreach (var userRole in userRolesToDelete)
+                foreach (var userRole in user.UserRoles.ToList())
                 {
-                    user.UserRoles.Remove(userRole);
-                    await _repository.SaveAsync();
+                    _repositoryUserRole.Delete(userRole);
                 }
+                await _repositoryUserRole.SaveAsync();
             }
 
             _repository.Delete(user);
             await _repository.SaveAsync();
 
-            return ApiResponse<string>.Success("کاربر حذف شد.");
+            return ApiResponse<string>.Success("کاربر با موفقیت حذف شد.");
         }
     }
 
